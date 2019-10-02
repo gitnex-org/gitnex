0bcbca8bdcbe41f0c5bc2f8c638819c43d87794c
M M Arif <mmarif@swatian.com>
2019-10-02 22:11:10 +0500

    Fix crash from gplay console, app crash when no avatar url node is present in API. old gitea instances.

--- app/src/main/java/org/mian/gitnex/adapters/FilesAdapter.java
+++ app/src/main/java/org/mian/gitnex/adapters/FilesAdapter.java
@@ -2,7 +2,6 @@ package org.mian.gitnex.adapters;
 
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
