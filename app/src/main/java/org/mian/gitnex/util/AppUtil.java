4fae271b09e502e0a3ac0c7c3712d76ec5aef5ec
M M Arif <mmarif@swatian.com>
2019-12-01 23:57:46 +0500

    allow copy, support more files

--- app/src/main/java/org/mian/gitnex/util/AppUtil.java
+++ app/src/main/java/org/mian/gitnex/util/AppUtil.java
@@ -218,7 +218,7 @@ public class AppUtil {
                 "coffee", "rc", "rs", "rust", "basic", "clj", "css", "dart", "lisp", "erl", "hs", "lsp", "rkt",
                 "ss", "llvm", "ll", "lua", "matlab", "pascal", "r", "scala", "sql", "latex", "tex", "vb", "vbs",
                 "vhd", "tcl", "wiki.meta", "yaml", "yml", "markdown", "xml", "proto", "regex", "py", "pl", "js",
-                "html", "htm", "volt", "ini", "htaccess", "conf", "gitignore"};
+                "html", "htm", "volt", "ini", "htaccess", "conf", "gitignore", "gradle", "txt", "properties", "bat"};
 
         return Arrays.asList(extValues).contains(ext);
 
