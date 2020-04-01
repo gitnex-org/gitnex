ee0392e208ee0ac15fd5d5d05b0da1add05b5afc
anonTree1417 <example@example.com>
2020-04-01 21:58:42 +0200

    Very small improvements

--- app/src/main/java/org/mian/gitnex/helpers/ssl/MTMDecision.java
+++ app/src/main/java/org/mian/gitnex/helpers/ssl/MTMDecision.java
@@ -8,7 +8,7 @@ class MTMDecision {
 
 	final static int DECISION_INVALID	= 0;
 	final static int DECISION_ABORT		= 1;
-	final static int DECISION_ALWAYS 	= 3;
+	final static int DECISION_ALWAYS 	= 2;
 
 	int state = DECISION_INVALID;
 
