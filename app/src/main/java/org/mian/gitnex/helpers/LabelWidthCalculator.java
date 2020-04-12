a7213add30e4a47d7d6097cb4b35f17ea5443cd6
opyale <example@example.com>
2020-04-12 20:33:41 +0200

    Adding more reliable calculation of label width.

--- app/src/main/java/org/mian/gitnex/helpers/LabelWidthCalculator.java
+++ app/src/main/java/org/mian/gitnex/helpers/LabelWidthCalculator.java
@@ -1,10 +1,27 @@
 package org.mian.gitnex.helpers;
 
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Typeface;
+
 /**
  * Author M M Arif
  */
 
 public class LabelWidthCalculator {
+
+    public static int calculateLabelWidth(String text, Typeface typeface, int textSize, int paddingLeftRight) {
+
+        Paint paint = new Paint();
+        Rect rect = new Rect();
+
+        paint.setTextSize(textSize);
+        paint.setTypeface(typeface);
+        paint.getTextBounds(text, 0, text.length(), rect);
+
+        return rect.width() + (paddingLeftRight * 2);
+
+    }
 
     public static int customWidth(int labelLength) {
 
