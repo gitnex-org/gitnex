d0fde4e7913814995d49d11277c6a796e39ff42c
opyale <opyale@noreply.codeberg.org>
2020-06-24 23:02:22 +0200

    Adjusting icon color to fit background. (#551)
    Adjusting icon color to fit background.
    
    Co-authored-by: opyale <opyale@noreply.gitea.io>
    Reviewed-by: M M Arif <mmarif@noreply.codeberg.org>

--- app/src/main/java/org/mian/gitnex/fragments/ProfileFragment.java
+++ app/src/main/java/org/mian/gitnex/fragments/ProfileFragment.java
@@ -1,6 +1,7 @@
 package org.mian.gitnex.fragments;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.widget.ImageViewCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
@@ -55,6 +57,7 @@ public class ProfileFragment extends Fragment {
         ImageView userAvatar = v.findViewById(R.id.userAvatar);
         TextView userLogin = v.findViewById(R.id.userLogin);
         TextView userLanguage = v.findViewById(R.id.userLanguage);
+        ImageView userLanguageIcon = v.findViewById(R.id.userLanguageIcon);
 
 	    ViewGroup aboutFrame = v.findViewById(R.id.aboutFrame);
 
@@ -84,6 +87,8 @@ public class ProfileFragment extends Fragment {
 				    userFullName.setTextColor(textColor);
 				    userLogin.setTextColor(textColor);
 				    userLanguage.setTextColor(textColor);
+
+				    ImageViewCompat.setImageTintList(userLanguageIcon, ColorStateList.valueOf(textColor));
 
 				    blurView.setupWith(aboutFrame)
 					    .setBlurAlgorithm(new RenderScriptBlur(ctx))
