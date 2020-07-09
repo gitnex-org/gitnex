1db6a3294be2ef3b616095601aca7a168a649ad5
opyale <opyale@noreply.codeberg.org>
2020-07-09 08:55:05 +0200

    Using GitLab CI. (#580)
    Using smaller image and publishing releases.
    
    Fixing things.
    
    Merge branch 'gitlab-ci' of https://codeberg.org/opyale/GitNex into gitlab-ci
    
    Removing eclint.
    
    Merge branch 'master' into gitlab-ci
    
    Only run jobs when master changed.
    
    Using GitLab CI.
    
    Co-authored-by: opyale <opyale@noreply.gitea.io>
    Reviewed-on: https://codeberg.org/gitnex/GitNex/pulls/580
    Reviewed-by: M M Arif <mmarif@noreply.codeberg.org>

--- scripts/sign-build.sh
+++ scripts/sign-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #make sure needed var's are here
-[ -z "${TOKEN}" ] && { echo "missing TOKEN"; exit 1; }
+[ -z "${BOT_TOKEN}" ] && { echo "missing BOT_TOKEN"; exit 1; }
 [ -z "${KS_PASS}" ] && { echo "missing KS_PASS"; exit 1; }
 [ -z "${KEY_PASS}" ] && { echo "missing KEY_PASS"; exit 1; }
 [ -z "${GITEA}" ] && { echo "missing GITEA"; exit 1; }
@@ -11,6 +11,6 @@
 
 
 KEYFILE=$(mktemp)
-curl -X GET "${GITEA}/api/v1/repos/${KS_REPO}/contents/${KS_FILE}?token=${TOKEN}" -H  "accept: application/json" | sed 's|"content":"|#|g' | cut -d '#' -f 2 | cut -d '"' -f 1 | base64 -d > ${KEYFILE}
+curl -X GET "${GITEA}/api/v1/repos/${KS_REPO}/contents/${KS_FILE}?token=${BOT_TOKEN}" -H  "accept: application/json" | sed 's|"content":"|#|g' | cut -d '#' -f 2 | cut -d '"' -f 1 | base64 -d > ${KEYFILE}
 
 /opt/android-sdk-linux/build-tools/*/apksigner sign -v --ks-pass pass:$KS_PASS --key-pass pass:$KEY_PASS --ks-key-alias GitNexBot --ks ${KEYFILE} --out signed.apk $(find . -name "*release*.apk")
