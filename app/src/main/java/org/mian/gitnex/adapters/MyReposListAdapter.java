7c7db79af597ca78e3384044606d644afd57de33
M M Arif <mmarif@noreply.codeberg.org>
2021-03-31 19:09:42 +0200

    [Backport] Fix lock icon for repos (#866)
    Fix lock icon for repos
    
    Fix layout
    
    Co-authored-by: M M Arif <mmarif@swatian.com>
    Reviewed-on: https://codeberg.org/gitnex/GitNex/pulls/866
    Reviewed-by: opyale <opyale@noreply.codeberg.org>
    Co-Authored-By: M M Arif <mmarif@noreply.codeberg.org>
    Co-Committed-By: M M Arif <mmarif@noreply.codeberg.org>

--- app/src/main/java/org/mian/gitnex/adapters/MyReposListAdapter.java
+++ app/src/main/java/org/mian/gitnex/adapters/MyReposListAdapter.java
@@ -292,7 +292,7 @@ public class MyReposListAdapter extends RecyclerView.Adapter<MyReposListAdapter.
 		}
 		holder.repoFullName.setText(currentItem.getFullName());
 		if(currentItem.getPrivateFlag()) {
-			holder.repoPrivatePublic.setImageResource(R.drawable.ic_lock);
+			holder.repoPrivatePublic.setVisibility(View.VISIBLE);
 			holder.repoType.setText(R.string.strPrivate);
 		}
 		else {
