e0ff35d530c346ddce0fcd47a7d1b416f0b74720
M M Arif <mmarif@noreply.codeberg.org>
2021-08-15 19:36:12 +0200

    [BUG] Releases should come before release tag (#958)
    As title
    
    Co-authored-by: M M Arif <mmarif@swatian.com>
    Reviewed-on: https://codeberg.org/gitnex/GitNex/pulls/958
    Co-authored-by: M M Arif <mmarif@noreply.codeberg.org>
    Co-committed-by: M M Arif <mmarif@noreply.codeberg.org>

--- app/src/main/java/org/mian/gitnex/activities/DeepLinksActivity.java
+++ app/src/main/java/org/mian/gitnex/activities/DeepLinksActivity.java
@@ -273,12 +273,12 @@ public class DeepLinksActivity extends BaseActivity {
 					new Handler(Looper.getMainLooper()).postDelayed(() ->
 						goToRepoSection(currentInstance, instanceToken, data.getPathSegments().get(0), data.getPathSegments().get(1), "newRelease"), 500);
 				}
-				else if(data.getPathSegments().get(2).equals("releases") && data.getPathSegments().get(3).equals("tag") && data.getPathSegments().size() == 5) { // release
-					repoIntent.putExtra("releaseTagName", data.getLastPathSegment());
+				else if(data.getPathSegments().get(2).equals("releases")) { // releases
 					new Handler(Looper.getMainLooper()).postDelayed(() ->
 						goToRepoSection(currentInstance, instanceToken, data.getPathSegments().get(0), data.getPathSegments().get(1), "releases"), 500);
 				}
-				else if(data.getPathSegments().get(2).equals("releases")) { // releases
+				else if(data.getPathSegments().get(2).equals("releases") && data.getPathSegments().get(3).equals("tag") && data.getPathSegments().size() == 5) { // release
+					repoIntent.putExtra("releaseTagName", data.getLastPathSegment());
 					new Handler(Looper.getMainLooper()).postDelayed(() ->
 						goToRepoSection(currentInstance, instanceToken, data.getPathSegments().get(0), data.getPathSegments().get(1), "releases"), 500);
 				}
