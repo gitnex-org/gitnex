758ee59a44bd8c3f3c61ba289c693ee1d0167e6d
M M Arif <mmarif@swatian.com>
2023-10-01 20:53:17 +0500

    Redesign create repo screen, add default branch, issue labels and license options

--- app/src/main/java/org/mian/gitnex/activities/CreateRepoActivity.java
+++ app/src/main/java/org/mian/gitnex/activities/CreateRepoActivity.java
@@ -1,22 +1,16 @@
 package org.mian.gitnex.activities;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.util.Log;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.regex.Pattern;
 import org.gitnex.tea4j.v2.models.CreateRepoOption;
 import org.gitnex.tea4j.v2.models.Organization;
@@ -26,7 +20,7 @@ import org.mian.gitnex.clients.RetrofitClient;
 import org.mian.gitnex.databinding.ActivityCreateRepoBinding;
 import org.mian.gitnex.helpers.AlertDialogs;
 import org.mian.gitnex.helpers.AppUtil;
-import org.mian.gitnex.helpers.Toasty;
+import org.mian.gitnex.helpers.SnackBar;
 import retrofit2.Call;
 import retrofit2.Callback;
 
@@ -38,103 +32,121 @@ public class CreateRepoActivity extends BaseActivity {
 	// https://github.com/go-gitea/gitea/blob/52cfd2743c0e85b36081cf80a850e6a5901f1865/models/repo.go#L964-L967
 	final List<String> reservedRepoNames = Arrays.asList(".", "..");
 	final Pattern reservedRepoPatterns = Pattern.compile("\\.(git|wiki)$");
-	public ImageView closeActivity;
 	List<String> organizationsList = new ArrayList<>();
-	private View.OnClickListener onClickListener;
-	private AutoCompleteTextView spinner;
-	private Button createRepo;
-	private EditText repoName;
-	private EditText repoDesc;
-	private CheckBox repoAccess;
+	List<String> issueLabelsList = new ArrayList<>();
+	List<String> licenseList = new ArrayList<>();
+	private ActivityCreateRepoBinding activityCreateRepoBinding;
 	private String loginUid;
 	private String selectedOwner;
-	private final View.OnClickListener createRepoListener = v -> processNewRepo();
+	private String selectedIssueLabels;
+	private String selectedLicense;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
 
-		ActivityCreateRepoBinding activityCreateRepoBinding =
-				ActivityCreateRepoBinding.inflate(getLayoutInflater());
+		activityCreateRepoBinding = ActivityCreateRepoBinding.inflate(getLayoutInflater());
 		setContentView(activityCreateRepoBinding.getRoot());
 
-		boolean connToInternet = AppUtil.hasNetworkConnection(ctx);
-
 		loginUid = getAccount().getAccount().getUserName();
-
-		InputMethodManager imm =
-				(InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-
-		closeActivity = activityCreateRepoBinding.close;
-		repoName = activityCreateRepoBinding.newRepoName;
-		repoDesc = activityCreateRepoBinding.newRepoDescription;
-		repoAccess = activityCreateRepoBinding.newRepoPrivate;
-
-		repoName.requestFocus();
-		assert imm != null;
-		imm.showSoftInput(repoName, InputMethodManager.SHOW_IMPLICIT);
-
-		initCloseListener();
-		closeActivity.setOnClickListener(onClickListener);
-
-		spinner = activityCreateRepoBinding.ownerSpinner;
 		getOrganizations(loginUid);
 
-		createRepo = activityCreateRepoBinding.createNewRepoButton;
-		disableProcessButton();
+		activityCreateRepoBinding.topAppBar.setNavigationOnClickListener(v -> finish());
 
-		if (!connToInternet) {
+		MenuItem attachment = activityCreateRepoBinding.topAppBar.getMenu().getItem(0);
+		attachment.setVisible(false);
+		MenuItem markdown = activityCreateRepoBinding.topAppBar.getMenu().getItem(1);
+		markdown.setVisible(false);
 
-			disableProcessButton();
-		} else {
+		String[] licenses = getResources().getStringArray(R.array.licenses);
+		Collections.addAll(licenseList, licenses);
+		getLicenses();
 
-			createRepo.setOnClickListener(createRepoListener);
-		}
+		issueLabelsList.add(getString(R.string.advanced));
+		issueLabelsList.add(getString(R.string.defaultText));
+		getIssueLabels();
+
+		activityCreateRepoBinding.topAppBar.setOnMenuItemClickListener(
+				menuItem -> {
+					int id = menuItem.getItemId();
+
+					if (id == R.id.create) {
+						processNewRepo();
+						return true;
+					} else {
+						return super.onOptionsItemSelected(menuItem);
+					}
+				});
 	}
 
 	private void processNewRepo() {
 
-		boolean connToInternet = AppUtil.hasNetworkConnection(appCtx);
-
-		String newRepoName = repoName.getText().toString();
-		String newRepoDesc = repoDesc.getText().toString();
-		boolean newRepoAccess = repoAccess.isChecked();
-
-		if (!connToInternet) {
-
-			Toasty.error(ctx, getResources().getString(R.string.checkNetConnection));
-			return;
-		}
+		String newRepoName =
+				Objects.requireNonNull(activityCreateRepoBinding.newRepoName.getText()).toString();
+		String newRepoDesc =
+				Objects.requireNonNull(activityCreateRepoBinding.newRepoDescription.getText())
+						.toString();
+		boolean newRepoAccess = activityCreateRepoBinding.newRepoPrivate.isChecked();
+		boolean repoAsTemplate = activityCreateRepoBinding.setAsTemplate.isChecked();
+		String defaultBranch =
+				Objects.requireNonNull(activityCreateRepoBinding.defaultBranch.getText())
+						.toString();
 
 		if (!newRepoDesc.equals("")) {
 
 			if (newRepoDesc.length() > 255) {
 
-				Toasty.warning(ctx, getString(R.string.repoDescError));
+				SnackBar.error(
+						ctx, findViewById(android.R.id.content), getString(R.string.repoDescError));
 				return;
 			}
 		}
 
 		if (newRepoName.equals("")) {
 
-			Toasty.error(ctx, getString(R.string.repoNameErrorEmpty));
+			SnackBar.error(
+					ctx,
+					findViewById(android.R.id.content),
+					getString(R.string.repoNameErrorEmpty));
 		} else if (!AppUtil.checkStrings(newRepoName)) {
 
-			Toasty.warning(ctx, getString(R.string.repoNameErrorInvalid));
+			SnackBar.error(
+					ctx,
+					findViewById(android.R.id.content),
+					getString(R.string.repoNameErrorInvalid));
 		} else if (reservedRepoNames.contains(newRepoName)) {
 
-			Toasty.warning(ctx, getString(R.string.repoNameErrorReservedName));
+			SnackBar.error(
+					ctx,
+					findViewById(android.R.id.content),
+					getString(R.string.repoNameErrorReservedName));
 		} else if (reservedRepoPatterns.matcher(newRepoName).find()) {
 
-			Toasty.warning(ctx, getString(R.string.repoNameErrorReservedPatterns));
+			SnackBar.error(
+					ctx,
+					findViewById(android.R.id.content),
+					getString(R.string.repoNameErrorReservedPatterns));
+		} else if (defaultBranch.equalsIgnoreCase("")) {
+
+			SnackBar.error(
+					ctx,
+					findViewById(android.R.id.content),
+					getString(R.string.repoDefaultBranchError));
 		} else if (selectedOwner == null) {
 
-			Toasty.error(ctx, getString(R.string.repoOwnerError));
+			SnackBar.error(
+					ctx, findViewById(android.R.id.content), getString(R.string.repoOwnerError));
 		} else {
 
-			disableProcessButton();
-			createNewRepository(loginUid, newRepoName, newRepoDesc, selectedOwner, newRepoAccess);
+			createNewRepository(
+					loginUid,
+					newRepoName,
+					newRepoDesc,
+					selectedOwner,
+					newRepoAccess,
+					defaultBranch,
+					repoAsTemplate);
 		}
 	}
 
@@ -143,7 +155,9 @@ public class CreateRepoActivity extends BaseActivity {
 			String repoName,
 			String repoDesc,
 			String selectedOwner,
-			boolean isPrivate) {
+			boolean isPrivate,
+			String defaultBranch,
+			boolean repoAsTemplate) {
 
 		CreateRepoOption createRepository = new CreateRepoOption();
 		createRepository.setAutoInit(true);
@@ -151,6 +165,10 @@ public class CreateRepoActivity extends BaseActivity {
 		createRepository.setPrivate(isPrivate);
 		createRepository.setReadme("Default");
 		createRepository.setName(repoName);
+		createRepository.setDefaultBranch(defaultBranch);
+		createRepository.setIssueLabels(selectedIssueLabels);
+		createRepository.setTemplate(repoAsTemplate);
+		createRepository.setLicense(selectedLicense);
 
 		Call<Repository> call;
 		if (selectedOwner.equals(loginUid)) {
@@ -164,7 +182,7 @@ public class CreateRepoActivity extends BaseActivity {
 		}
 
 		call.enqueue(
-				new Callback<Repository>() {
+				new Callback<>() {
 
 					@Override
 					public void onResponse(
@@ -175,31 +193,59 @@ public class CreateRepoActivity extends BaseActivity {
 
 							MainActivity.reloadRepos = true;
 							OrganizationDetailActivity.updateOrgFABActions = true;
-							Toasty.success(ctx, getString(R.string.repoCreated));
-							enableProcessButton();
-							finish();
+
+							SnackBar.success(
+									ctx,
+									findViewById(android.R.id.content),
+									getString(R.string.repoCreated));
+
+							new Handler().postDelayed(() -> finish(), 3000);
 						} else if (response.code() == 401) {
 
-							enableProcessButton();
 							AlertDialogs.authorizationTokenRevokedDialog(ctx);
 						} else if (response.code() == 409) {
 
-							enableProcessButton();
-							Toasty.warning(ctx, getString(R.string.repoExistsError));
+							SnackBar.error(
+									ctx,
+									findViewById(android.R.id.content),
+									getString(R.string.repoExistsError));
 						} else {
 
-							enableProcessButton();
-							Toasty.error(ctx, getString(R.string.genericError));
+							SnackBar.error(
+									ctx,
+									findViewById(android.R.id.content),
+									getString(R.string.genericError));
 						}
 					}
 
 					@Override
-					public void onFailure(@NonNull Call<Repository> call, @NonNull Throwable t) {
-
-						Log.e("onFailure", t.toString());
-						enableProcessButton();
-					}
+					public void onFailure(@NonNull Call<Repository> call, @NonNull Throwable t) {}
 				});
+	}
+
+	private void getIssueLabels() {
+
+		ArrayAdapter<String> adapter =
+				new ArrayAdapter<>(
+						CreateRepoActivity.this, R.layout.list_spinner_items, issueLabelsList);
+
+		activityCreateRepoBinding.issueLabels.setAdapter(adapter);
+
+		activityCreateRepoBinding.issueLabels.setOnItemClickListener(
+				(parent, view, position, id) ->
+						selectedIssueLabels = issueLabelsList.get(position));
+	}
+
+	private void getLicenses() {
+
+		ArrayAdapter<String> adapter =
+				new ArrayAdapter<>(
+						CreateRepoActivity.this, R.layout.list_spinner_items, licenseList);
+
+		activityCreateRepoBinding.licenses.setAdapter(adapter);
+
+		activityCreateRepoBinding.licenses.setOnItemClickListener(
+				(parent, view, position, id) -> selectedLicense = licenseList.get(position));
 	}
 
 	private void getOrganizations(final String userLogin) {
@@ -208,7 +254,7 @@ public class CreateRepoActivity extends BaseActivity {
 				RetrofitClient.getApiInterface(ctx).orgListCurrentUserOrgs(1, 50);
 
 		call.enqueue(
-				new Callback<List<Organization>>() {
+				new Callback<>() {
 
 					@Override
 					public void onResponse(
@@ -247,9 +293,9 @@ public class CreateRepoActivity extends BaseActivity {
 											R.layout.list_spinner_items,
 											organizationsList);
 
-							spinner.setAdapter(adapter);
+							activityCreateRepoBinding.ownerSpinner.setAdapter(adapter);
 
-							spinner.setOnItemClickListener(
+							activityCreateRepoBinding.ownerSpinner.setOnItemClickListener(
 									(parent, view, position, id) ->
 											selectedOwner = organizationsList.get(position));
 
@@ -260,7 +306,7 @@ public class CreateRepoActivity extends BaseActivity {
 								new Handler(Looper.getMainLooper())
 										.postDelayed(
 												() -> {
-													spinner.setText(
+													activityCreateRepoBinding.ownerSpinner.setText(
 															organizationsList.get(selectOwnerById),
 															false);
 													selectedOwner =
@@ -270,36 +316,15 @@ public class CreateRepoActivity extends BaseActivity {
 								getIntent().removeExtra("organizationAction");
 							}
 
-							enableProcessButton();
 						} else if (response.code() == 401) {
 
-							enableProcessButton();
 							AlertDialogs.authorizationTokenRevokedDialog(ctx);
 						}
 					}
 
 					@Override
 					public void onFailure(
-							@NonNull Call<List<Organization>> call, @NonNull Throwable t) {
-
-						Log.e("onFailure", t.toString());
-						enableProcessButton();
-					}
+							@NonNull Call<List<Organization>> call, @NonNull Throwable t) {}
 				});
-	}
-
-	private void initCloseListener() {
-
-		onClickListener = view -> finish();
-	}
-
-	private void disableProcessButton() {
-
-		createRepo.setEnabled(false);
-	}
-
-	private void enableProcessButton() {
-
-		createRepo.setEnabled(true);
 	}
 }
